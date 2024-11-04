e71b07d16af14f3567b9ae4aff803c7a440119c2
Bamidele Ogunseye <bambamogunseye@gmail.comgit config --user.name bamidele>
2024-11-04 17:43:24 -0600

    full commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import CommentSection from './components/CommentSection'
+import './App.css'
+
+function App() {
+ return (
+  <>
+  <h1>Leave a Comment Here!</h1>
+  <CommentSection />
+  </>)
+}
+export default App
